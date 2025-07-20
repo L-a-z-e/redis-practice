4b846e6a22bd7297bef11ef6273bc3bcfd4beb20
L-a-z-e <yysi8771@gmail.com>
2025-07-20 13:10:11 +0900

    springboot redis pubsub

---
+++ redis-pubsub/src/main/java/com/laze/redispubsub/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.laze.redispubsub;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    MessageListenerAdapter listenerAdapter() {
+        return new MessageListenerAdapter(new MessageListenService());
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisMessageListenerContainer (RedisConnectionFactory redisConnectionFactory, MessageListenerAdapter listenerAdapter) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(listenerAdapter, ChannelTopic.of("users:unregister"));
+
+        return container;
+    }
+
+}
---
+++ redis-session/src/main/java/com/laze/redissession/RedisSessionApplication.java
@@ -0,0 +1,30 @@
+package com.laze.redissession;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@SpringBootApplication
+public class RedisSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSessionApplication.class, args);
+	}
+
+	@GetMapping
+	public Map<String, String> home(HttpSession session) {
+		Integer visitCount = (Integer)  session.getAttribute("visits");
+		if (visitCount == null) {
+			visitCount =0 ;
+		}
+
+		session.setAttribute("visits", ++visitCount);
+
+		return Map.of("session id", session.getId(), "visits", visitCount.toString());
+	}
+}
